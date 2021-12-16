2903af93e0251fe2a29fdb97f9237d46c5a0d263
Roberto Arista <arista.rob@gmail.com>
2021-12-16 14:58:04 +0100

    adapting to the extension

--- build.py
+++ build.py
@@ -1,7 +1,6 @@
-'''build RoboFont Extension'''
+'''build CAM Simulator extension'''
 
 import os
-from AppKit import NSCommandKeyMask, NSAlternateKeyMask, NSShiftKeyMask
 from mojo.extensions import ExtensionBundle
 
 # get current folder
@@ -13,9 +12,6 @@ sourcePath = os.path.join(basePath, 'source')
 # folder with python files
 libPath = os.path.join(sourcePath, 'code')
 
-# folder with html files
-htmlPath = os.path.join(sourcePath, 'documentation')
-
 # folder with resources (icons etc)
 resourcesPath = os.path.join(sourcePath, 'resources')
 
@@ -23,11 +19,8 @@ resourcesPath = os.path.join(sourcePath, 'resources')
 # see choosealicense.com for more open-source licenses
 licensePath = os.path.join(basePath, 'license.txt')
 
-# required extensions
-requirementsPath = os.path.join(basePath, 'requirements.txt')
-
 # name of the compiled extension file
-extensionFile = 'myExtension.roboFontExt'
+extensionFile = 'CAMSimulator.roboFontExt'
 
 # path of the compiled extension
 buildPath = os.path.join(basePath, 'build')
@@ -37,62 +30,47 @@ extensionPath = os.path.join(buildPath, extensionFile)
 B = ExtensionBundle()
 
 # name of the extension
-B.name = "myExtension"
+B.name = "CAM Simulator"
 
 # name of the developer
-B.developer = 'RoboDocs'
+B.developer = 'Roberto Arista'
 
 # URL of the developer
-B.developerURL = 'http://github.com/roboDocs'
+B.developerURL = 'https://github.com/roberto-arista'
 
 # extension icon (file path or NSImage)
 imagePath = os.path.join(resourcesPath, 'icon.png')
 B.icon = imagePath
 
 # version of the extension
-B.version = '0.2.6'
+B.version = '0.2.0'
 
 # should the extension be launched at start-up?
 B.launchAtStartUp = True
 
 # script to be executed when RF starts
-B.mainScript = 'hello.py'
-
-# does the extension contain html help files?
-B.html = True
+B.mainScript = 'events.py'
 
 # minimum RoboFont version required for this extension
 B.requiresVersionMajor = '4'
-B.requiresVersionMinor = '0'
+B.requiresVersionMinor = '1'
 
 # scripts which should appear in Extensions menu
 B.addToMenu = [
     {
-        'path':          'doSomething.py',
-        'preferredName': 'do something',
-        'shortKey':      (NSCommandKeyMask | NSShiftKeyMask, 'b'),
+        'path':          'camSimulator.py',
+        'preferredName': 'CAM Simulator',
+        'shortKey':      '',
     },
-    {
-        'path':          'doSomethingElse.py',
-        'preferredName': 'do something else',
-        'shortKey':      (NSAlternateKeyMask, 'o'),
-    }
 ]
 
 # license for the extension
 with open(licensePath) as license:
     B.license = license.read()
 
-# required extensions
-with open(requirementsPath) as requirements:
-    B.requirements = requirements.read()
-
-# expiration date for trial extensions
-B.expireDate = '2020-12-31'
-
 # compile and save the extension bundle
 print('building extension...', end=' ')
-B.save(extensionPath, libPath=libPath, htmlPath=htmlPath, resourcesPath=resourcesPath, pycOnly=pycOnly)
+B.save(extensionPath, libPath=libPath, resourcesPath=resourcesPath)
 print('done!')
 
 # check for problems in the compiled extension
